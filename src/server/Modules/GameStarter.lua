37028357dddb1295c9e53d978b5458e85e28b08b
LIGHTFALL <bv2010cc@outlook.com>
2025-07-30 17:19:06 -0600

    e

---
+++ src/server/Modules/GameStarter.lua
@@ -0,0 +1,19 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ServerScriptService = game:GetService("ServerScriptService")
+
+local Remotes = ReplicatedStorage.Remotes
+local Server = ServerScriptService.Server
+
+local Modules = Server.Modules
+local Components = Modules.Components
+
+
+local GameStarter = {}
+
+
+
+function GameStarter:Initialize()
+
+end
+
+return GameStarter
